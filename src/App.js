d629c9146c5ff20896ab3594cbb89ce00992f78c
Kyle Nguyen <nghia002@outlook.com>
2021-03-13 14:23:56 +0800

    feat: play sound on display char

--- src/App.js
+++ src/App.js
@@ -4,25 +4,53 @@ import { useState, useEffect } from 'react';
 
 function fetchData(setWordList) {
     fetch("http://localhost:3000/")
-        .then(res => res.json())
+        .then(res => {
+            return res.json()
+        })
         .then(data => {
-            console.log(data);
+            data.forEach(element => {
+                if (element.pronunciation) console.log(element.pronunciation.data);
+                const buffer = element.pronunciation ? Buffer.from(element.pronunciation.data) : null;
+                element.pronunciation = buffer;
+            });
             setWordList(data);
         })
         .catch(err => console.log(err));
 }
 
+function PlaySound(props) {
+    const b64 = props.data.toString('base64');
+    useEffect(() => {
+        const audioElement = document.getElementById('playsound');
+        audioElement.play();
+        return () => {
+            console.log(audioElement);
+            audioElement.pause();
+            audioElement.currentTime = 0;
+        };
+    });
+    return (
+        <audio id='playsound'>
+            <source src={"data:audio/x-wav;base64," + (b64)} ></source>
+        </audio>
+    );
+}
 function DisplayChar(props) {
     const [currentIdx, setIdx] = useState(0);
     useEffect(() => {
-        const interval = setInterval(() => setIdx(currentIdx => (currentIdx+1)%props.wordList.length), 500);
+        const interval = setInterval(() => setIdx(currentIdx => (currentIdx + 1) % props.wordList.length), 500);
         console.log(interval);
         return () => {
             clearInterval(interval);
         };
     }, []);
     return (
-        <div className='Char'>{props.wordList[currentIdx]?.char}</div>
+        <div>
+            <div className='Char'>{props.wordList[currentIdx]?.char}</div>
+            {props.wordList[0]?.pronunciation ?
+                <PlaySound data={props.wordList[0]?.pronunciation}></PlaySound>
+                : ''}
+        </div>
     )
 }
 
@@ -31,7 +59,7 @@ function App() {
     useEffect(() => fetchData(setWordList), []);
 
     return (
-        <div className='App-header'>{wordList?.length? <DisplayChar wordList={wordList}></DisplayChar>: ''}</div>
+        <div className='App-header'>{wordList?.length ? <DisplayChar wordList={wordList}></DisplayChar> : ''}</div>
     );
 }
 
